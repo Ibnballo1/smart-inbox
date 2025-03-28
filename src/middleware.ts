5282e8be373766aaa23c355dfc9e3c7fd744b641
Abdullateef Bello <belloabdullateef035@gmail.com>
2025-03-28 16:12:22 +0100

    setup database

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,6 +2,11 @@ import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
 const isPublicRoute = createRouteMatcher(["/sign-in(.*)", "/sign-up(.*)"]);
 
+// The clerkMiddleware function is a middleware that protects your application routes.
+// It checks if the user is authenticated and redirects them to the sign-in page if they are not.
+// The `isPublicRoute` function is used to determine if the current route is public
+// (i.e., does not require authentication).
+// If the route is public, the middleware does not protect it.
 export default clerkMiddleware(async (auth, req) => {
   if (!isPublicRoute(req)) {
     await auth.protect();
